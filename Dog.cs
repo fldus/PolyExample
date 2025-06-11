abb12ca4c925654273d7afb54cb7055b4814370f
Kim Riyeon <kry59965@gmail.com>
2025-06-11 14:01:09 +0900

    강아지 고양이 클래스 예제

---
+++ Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyExample
+{
+    internal class Dog
+    {
+        public Dog() { this.Age = 0; }
+
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+        public void Eat() { Console.WriteLine("냠냠 먹습니다."); }
+        public void Sleep() { Console.WriteLine("쿨쿨 잠을 잡니다."); }
+        public void Bark() { Console.WriteLine("왈왈 짓습니다."); }
+    }
+}
